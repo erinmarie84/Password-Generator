d890635615fea7b062966e569f079a3ad62756a2
Erin Frasier <e_marie84@ymail.com>
2020-07-10 21:10:22 -0700

    I think I got it

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Password-Generator
 Homework 3
+Use the given HTML layout and create checkboxes for users to choose which elements they want their password to inlcude
+Use javascript to create variables for each type of password element
+
+
+
+Psuedocode
+
+Creat variables for each type of password criteria 
+
+Create prompt/confirm for:
+    Uppercase
+    Lowercase
+    Numbers
+    Special Characters
+    Length of password form 8-128
+
+Store the information that the user chooses 
+    
+    
+
+
--- script.js
+++ script.js
@@ -1,36 +1,74 @@
-function generate (){
-
-  var lowercase = 'abcdefghijklmnopqrstuvxwyz'
-  var uppercase = 'ABCDEFGH'
-  var numbers = '0123'
-  var special characters = '!'
-
-  var lowercase
-
-  var userpassword = '';
-  var passwordCharset = '';
-
-  if (lowercaseInput.checked) {
-
-  }
-
-  for (var i = 0; i < passlenght; i++) {
-    userpassword += passwordCharset.charAt(Math.floor(Math.random() *)
-  }
-
-  document.getElementById
-}
-
-
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate');
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  var charNum= prompt('How many characters do you want your password to use?');
+  // Verify how many characters user wants and start over if they choose too many or too little characters
+  if(charNum <8 || charNum >128){
+    return writePassword();
+  }
+
+  // Create variables to store users input/answers
+
+  var uppercase = confirm('Do you want to include uppercase characters?');
+  var lowercase = confirm('Do you want to include lowercase characters?');
+  var numbers = confirm('Do you want to include numbers?');
+  var specialChar = confirm('Do you want special characters?');
+
+  // Create array's for each variable to pull from
+
+  var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+  var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+  var numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+  var specialCharArray = ['!', '"','#', '$', '%', '&', '(',')','*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
+
+  // Create empty array to hold users chosen characters
+
+  var emptyArray = [];
+
+  if(uppercase) {
+    emptyArray.push(...upperArray);
+  } 
+
+  if(lowercase) {
+    emptyArray.push(...lowerArray);
+  }
+
+  if(numbers) {
+    emptyArray.push (...numbersArray);
+  }
+
+  if(specialChar) {
+    emptyArray.push (...specialCharArray);
+  }
+
+ // Create variable to push the password into
+
+  var passArray = [];
+
+  // Create for loop with math.floor so that the computer will only choose the number of characters that the user chose and ramdomly pick the characters and put that information into the second empty array.
+
+  for(let i = 0; i < charNum; i++){
+    passArray.push(emptyArray[Math.floor(Math.random() * emptyArray.length)]);
+  
+  }
+
+  // Create a placeholder variable attachted to an html element
+
+  var securePassBox = document.getElementById('password');
+
+  // Use .value so that the computer puts the information from passArray into the generate password box
+
+  securePassBox.value = passArray.join(''); 
+  
 
 }
 
